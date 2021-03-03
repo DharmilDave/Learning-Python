c1780705f4fbd7cebca59bacedcb9336302609f8
Virus <dharmil.j.dave@gamil.com>
2021-03-03 15:53:32 +0530

    description for init

--- Tic Tac Toe/GUI.py
+++ Tic Tac Toe/GUI.py
@@ -6,7 +6,7 @@ from pygame.locals import *
 XO = "x"
 winner = None
 draw = None
-height, width = 400, 400
+width, height = 400, 400
 white = (255, 255, 255)
 line_color = (0, 0, 0)
 board = [[None]*3, [None]*3, [None]*3]
@@ -19,13 +19,18 @@ pg.init()
 fps = 30
 
 CLOCK = pg.time.Clock()
-
+# * To set up our display window: pg.display.set_moded((tuple having width & height),depth,fps)
 screen = pg.display.set_mode((width, height+100), 0, 32)
+# to give name to our display
 pg.display.set_caption("Tic Tac Toe Game")
+# to load the background image to customize the display
+# the problem is it takes img in its native size so we need to transformit
 initializing_window = pg.image.load("img\cover.png")
 x_img = pg.image.load("img\X.png")
 o_img = pg.image.load("img\O.png")
 
+# here we will transform the loaded images to our specific scale
+# * pg.transform.scale(name of img,(width, height))
 initializing_window = pg.transform.scale(
     initializing_window, (width, height+100))
 x_img = pg.transform.scale(x_img, (80, 80))
