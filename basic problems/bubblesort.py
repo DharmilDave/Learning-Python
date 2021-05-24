a919cb87c3ff3e5c4060ce8eaa8be1454e807cfe
DharmilDave <dharmil.j.dave@gmail.com>
2021-05-24 18:40:50 +0530

    added counts

--- basic problems/bubblesort.py
+++ basic problems/bubblesort.py
@@ -1,15 +1,17 @@
 def bubble_sort(arr):
+    c = 0
     for i in range(len(arr)):
         swapped = False
         for j in range(len(arr)-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 swapped = True
+                c += 1
 
         if swapped == False:
             break
-
-    print(arr)
+    print(f"Total {c} swaps required to sort the array.")
+    print("Sorted array: {arr}")
 
 
 arr = [5, 3, 18, 14, 6, 2]
