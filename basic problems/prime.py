8ee17cb1589a1ecd9a0bee01a40429a72c96c094
DharmilDave <dharmil.j.dave@gmail.com>
2021-05-25 20:58:03 +0530

    prime code modified

--- basic problems/prime.py
+++ basic problems/prime.py
@@ -2,24 +2,17 @@ import math
 
 
 def is_prime(n):
-    is_p = False
-    if n <= 0:
-        print("not valid")
-    elif n == 1:
-        print("not defined")
-    elif n == 2:
-        print("prime")
+    if n <= 1:
+        return False
     else:
-        sqr = int(math.sqrt(n))
-        for i in range(2, sqr):
+        for i in range(2, int(math.sqrt(n))):
             if n % i == 0:
-                is_p = True
-                break
-        if is_p:
-            print("prime")
-        else:
-            print("not prime")
+                return False
+    return True
 
 
 n = int(input("Enter a number to check : "))
-is_prime(n)
+if is_prime(n):
+    print("prime")
+else:
+    print("Not prime")
